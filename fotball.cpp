3eecdc94d2527e9b79c5a754787299d51fb609bc
julian53 <julher2000@hotmail.com>
2019-11-19 22:16:07 -0500

    arreglando el archivo futball.cpp

---
+++ fotball.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+int futball (){
+    string local="s",visitante="s",gpp="si",got="hola",lov="no";
+    float gecl=0,gecv=0,promgl=0,promgv=0;
+    int resultado1=0,l=1,resultado2=0,opcion=0,posicion=0;
+    int v[]={1,2,3},n[]={1,2,3},gppl[]={1,2,3,4},gppv[]={1,2,3,4};
+	cout<<"PROGRAMA QUE PREDICE UN RESULTADO DE FUTBALL";
+    cout<<"digite el nombre del equipo local y luego el del visitante\n";
+    cin>>local>>visitante;
+    for (int i=0;i<=2;i++){
+        switch(opcion=i){
+            case 0:
+                cout<<"digite el promedio de goles por partido \ndel equipo LOCAL y luego del VISITANTE\n";
+                cin>>v[i]>>n[i];
+                resultado1=(v[i]+n[i])/2;
+                if (resultado1>=6){
+                    gpp="se puede apostar al over +4.5 [[]]SUERTE CRACK[[]]";
+                }
+                else{
+                    for (int j=0;j<=3;j++){
+                    cout<<"digita los goles totales de el partido #"<<l<<" \n que se jugaron en las ultimas fechas  de \n el LOCAL y el VISITANTE luego. \n";
+                    cin>>gppl[j]>>gppv[j];
+                    if ((gppl[j]<=7)&&(gppv[j]<=7)){
+                    	resultado2=resultado2+1;
+                    }
+                    if (resultado2>=3){
+                    	gpp="se puede apostar al under -7.5 [[]]SUERTE CRACK[[]]";
+					}
+					else{
+						gpp="no apueste ni por el under ni por el over [[]]SUERTE CRACK[[]]";
+					}
+                    l=l+1;
+                    }
+                }
+            break;
+            
+            case 1:
+                cout<<"digite la cantidad de goles anotados en la \ncompetencia de el LOCAL y luego del VISITANTE. \n";
+				cin>>v[i]>>n[i];
+                cout<<"digite la cantidad de goles encontra de el \nequipo LOCAL y luego del VISITANTE\n";
+                cin>>gecl>>gecv;
+                promgl=v[i]-gecl;
+                promgv=n[i]-gecv;
+                if((promgl>=0)&&(promgv>=0)){
+                    if(promgl>=(promgv*2)){
+                        got="gana el equipo LOCAL [[]]SUERTE CRACK[[]]";
+                    }
+                    else {
+                         if(promgv>=(promgl*2)){
+                        got="gana el equipo VISITANTE [[]]SUERTE CRACK[[]]";
+                        }
+                        else{
+                        got="los dos equipos hacen almenos un gol SI [[]]SUERTE CRACK[[]]";
+                        }
+                    }
+                }
+                else{
+                    if((promgv<0)&&(promgl>=0)){
+                        if(promgl>=(-(promgv*2))){
+                            got="gana el equipo LOCAL [[]]SUERTE CRACK[[]]";
+                        }
+                        else{
+                            got="los dos equipos hacen almenos un gol SI [[]]SUERTE CRACK[[]]";
+                        }  
+                    }
+                     else{
+                        if(promgv>=(-(promgl*2))){
+                            got="gana el equipo VISITANTE [[]]SUERTE CRACK[[]]";
+                        }
+                        else{
+                            got="los dos equipos hacen almenos un gol SI [[]]SUERTE CRACK[[]]";
+                        }
+                    }
+                }
+            break;
+
+            case 2:
+            	cout<<"digite la posicion de el equipo LOCAL y el \nVISITANTE en la calsificacion general\n";
+            	cin>>v[i]>>n[i];
+            	if (v[i]>n[i]){
+            		posicion=v[i]-n[i];
+            		if (posicion>7){
+						lov="no apueste al handicap";
+					}
+					else{
+						lov="apueste al hadicap +1.5 o +1 al LOCAL\n o los dos equipos hacen gol [[]]SUERTE CRACK[[]]";
+					}
+				}
+				else {
+					posicion=n[i]-v[i];
+						if (posicion>7){
+						lov="no apueste al hadicap [[]]SUERTE CRACK[[]]";
+					}
+					else{
+						lov="apueste al hadicap +1.5 o +1 al LOCAL [[]]SUERTE CRACK[[]]";
+					}
+				}
+			break;
+			
+			default: cout<<"adios espero que le sirva ;v";
+			;
+        }
+        opcion=opcion+1;
+    }
+    
+    cout<<"entre el enfrentamiento de "<<local<<" vs "<<visitante<<"\n"<<gpp<<"\n"<<got<<"\n"<<lov;	
+}
